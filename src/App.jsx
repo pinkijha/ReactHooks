2d481292e948eaef4c28785625b3ab905213368a
Pinki Jha <pinkijha1310@gmail.com>
2024-11-19 20:53:57 +0530

    decrease and increase count using useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState('Blue')
+  const [count, setCount] = useState(0);
 
   const clickHandler = () => {
     
-    setColor(color === 'Blue' ? 'Red' : 'Blue');
-
+    setCount(count+1);
   }
+
+    const clickHandler1 = () => {
+    
+      setCount(count-1);
+  
+    }
+  
  
 
   return (
     <>
-      <h1>My favourite color is {color}</h1>
-      <button onClick={clickHandler} >{color === 'Blue' ? 'Red' : 'Blue'}</button>
+      <h1>Count: {count} </h1>
+      <button onClick={clickHandler} > Increase </button>
+      <button onClick={clickHandler1} > Decrease</button>
     </>
   )
 }
