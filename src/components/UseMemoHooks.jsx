27cdbed589f819f8207fe38e41f4a135f59cc360
Pinki Jha <pinkijha1310@gmail.com>
2024-11-21 08:26:11 +0530

    create components with using useMemo

--- src/components/UseMemoHooks.jsx
+++ src/components/UseMemoHooks.jsx
@@ -1,47 +1,62 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from "react";
 
 const UseMemoHooks = () => {
-    const [search, setSearch] = useState('');
-    const [cartCount, setCartCount] = useState(0);
-    const productList =[
-       'Mobile',
-       'Car',
-       'Charger',
-       'Laptop',
-       'MacBook',
-       'Headphone',
-        'AirPods',
-        'Monitor',
-    ]
+  const [search, setSearch] = useState("");
+  const [cartCount, setCartCount] = useState(0);
+  const productList = [
+    "Mobile",
+    "Car",
+    "Charger",
+    "Laptop",
+    "MacBook",
+    "Headphone",
+    "AirPods",
+    "Monitor",
+  ];
 
-    const serachProduct = (e) => {
-        setSearch(e.target.value);
-        console.log(search)
-    }
+  const serachProduct = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const ClickHandler = (() =>{
-       setCartCount(cartCount+1)
-    })
+  const increment = () => {
+    setCartCount(cartCount + 1);
+  };
 
-    // filter Products
-    const filterProduct = productList.filter((pro) => {
-        return pro.toLowerCase().includes(search.toLowerCase());
-    })
+  const decrement = () => {
+    setCartCount(cartCount - 1);
+  };
 
-    console.log("filtering Product...")
+  // filter Products
+  const filteredProducts = useMemo(() => {
+    console.log("filtering Product...");
+    return productList.filter((product) => {
+        return product.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [search]);
 
   return (
     <div>
       <h1>Search Products</h1>
-      <input placeholder='serch product' value={search} onChange={serachProduct} type='text' />
-      <button onClick={ClickHandler}> Add to cart ({cartCount})</button>
+      <input
+        placeholder="serch product"
+        value={search}
+        onChange={serachProduct}
+        type="text"
+      />
+      <button onClick={decrement}> - </button>
+      <span> Add to cart ({cartCount}) </span>{" "}
+      <button onClick={increment}>+</button>
       <ul>
-        {filterProduct.map((product,index)=>(
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product, index) => (
             <li key={index}>{product}</li>
-        ))}
+          ))
+        ) : (
+          <p>No products found.</p>
+        )}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default UseMemoHooks
+export default UseMemoHooks;
